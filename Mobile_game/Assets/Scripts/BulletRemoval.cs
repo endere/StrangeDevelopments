e0ab3176ce81356dd68b0b9771cd98f7072b1049
endere <erik.end@gmail.com>
2017-08-09 14:37:39 -0700

    enemies

---
+++ Mobile_game/Assets/Scripts/BulletRemoval.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletRemoval : MonoBehaviour {
+	public float despawnTime;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (despawnTime > 0)
+		{
+			despawnTime -= Time.deltaTime;
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+		
+	}
+}
---
+++ Mobile_game/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	// // Use this for initialization
+	// void Start () {
+		
+	// }
+	
+	// // Update is called once per frame
+	// void Update () {
+	// }
+	public float playerHealth;
+	void OnCollisionEnter2D(Collision2D collision)
+	{	
+		if (collision.gameObject.tag=="Bullet")
+		{
+			playerHealth -= 1;
+			Destroy(collision.gameObject);
+			if (playerHealth == 0)
+            {
+                Destroy(this.gameObject);
+            }
+
+		}
+	}
+}
