b7c12fba95642399b3da340c7c4c73cfbb4ce9d0
endere <erik.end@gmail.com>
2017-08-29 15:05:52 -0700

    normalized stalker speed

--- Mobile_game/Assets/Scripts/follow.cs
+++ Mobile_game/Assets/Scripts/follow.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class follow : MonoBehaviour {
 	// public GameObject target;
 	public float speed;
-	public float rotationSpeed;
 	private Vector3 target;
 	// Use this for initialization
 	void Start () {
@@ -15,8 +15,13 @@ public class follow : MonoBehaviour {
 	void Update () {
 		target = GameObject.Find("Player").transform.position;
 		Vector3 toTarget = target - transform.position;
-		transform.Translate(toTarget * speed * Time.deltaTime);
-		// Debug.Log(target.gameObject.name);
+		float mod = Math.Abs(toTarget[0]) + Math.Abs(toTarget[1]);
+		transform.Translate(toTarget * (speed/mod) * Time.deltaTime);
+		//to have them slow down as they approach the player, instead of travel
+		//at a constant speed. take out the mod, and make the calculation 
+		// totarget * speed * time.deltatime
+		// Debug.Log(toTarget * speed * Time.deltaTime);
+		// GetComponent<Rigidbody2D>().velocity = speed;
 		// Debug.Log(Quaternion.Slerp(transform.position, target.transform.position, rotationSpeed*Time.deltaTime));
 	}
 }
