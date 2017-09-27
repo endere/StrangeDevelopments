8b9ae85bf9eadd4e5d82233280e5a832efb564fa
endere <erik.end@gmail.com>
2017-09-26 19:37:58 -0700

    creating a test to show other people how acp works

---
+++ Mobile_game/Assets/Scripts/test script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//this is some amazing code that completely works and is not broken at all.
+	}
+}
